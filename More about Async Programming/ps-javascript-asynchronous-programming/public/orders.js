c454771debf241b5c8049990aac7295a08447cc6
Anoop Pemmaraju <anuppemmaraju@outlook.com>
2026-06-18 23:13:54 +0530

    Promises and Chaining Promises.

---
+++ More about Async Programming/ps-javascript-asynchronous-programming/public/orders.js
@@ -0,0 +1,27 @@
+// axios
+//   .get("http://localhost:3000/api/orders")
+//   .then(({ data }) => {
+//     showOrderList("#order-list", data);
+//   })
+//   .catch((err) => showError("#order-list", err));
+let statuses = [];
+showWaiting();
+axios
+  .get("http://localhost:3000/api/orderStatuses")
+  .then(({ data }) => {
+    statuses = data;
+    return axios.get("http://localhost:3000/api/orders");
+  })
+  .then(({ data }) => {
+    let orders = data.map((o) => {
+      return {
+        ...o,
+        orderStatus: statuses.find((d) => d.id === o.orderStatusId).description,
+      };
+    });
+    showOrderList("#order-list", orders);
+  })
+  .catch((err) => showError("#order-list", err))
+  .finally(() => {
+    setTimeout(hideWaiting, 1500);
+  });
